474071fb901d0c889062de08a3058fa6b217ae4c
Smriti <smritikshetri66@gmail.com>
2026-05-06 14:37:13 +0200

    Adding name to the list

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ eleves = [
      {"prenom": "Chahinez", "nom": "Boubekeur", "github": "mariabkr26", "groupe": "Groupe 1"},
     {"prenom": "Alake Faridath", "nom": "ABOGOURIN", "github": "faridathabogourin", "groupe": "Gr1"},
     {"prenom": "Nora", "nom": "Boukal", "github": "loic-prof"},
+    {"prenom": "Smriti", "nom": "G.C.", "github": "smritikshetri55"},
 ]
     
 TEMPLATE = """
